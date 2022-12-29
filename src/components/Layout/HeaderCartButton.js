43d18634ddf68b30472415e8cf422b7527fdd2d2
Scott Peat <scottpeatweb@gmail.com>
2022-12-29 21:05:09 +0000

    Starting creating the HeaderCartIcon file

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const HeaderCartButton = (props) => {
+  return (
+    <button>
+      <span></span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
