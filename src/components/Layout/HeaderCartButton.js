88394bb0d3253f69527abc7b48aba5bdd5930213
Scott Peat <scottpeatweb@gmail.com>
2022-12-30 11:34:16 +0000

    Added Styled Componenets for keyframes animation for Cart Button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,71 +1,79 @@
-import styled from 'styled-components';
+import CartIcon from '../Cart/CartIcon';
+import styled, { keyframes } from 'styled-components';
+import HeaderCartButton from '../Layout/Header';
 
 const HeaderCartButton = (props) => {
   return (
-    <button>
-      <span></span>
-    </button>
+    <Button>
+      <span>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span>3</span>
+    </Button>
   );
 };
 
 export default HeaderCartButton;
 
-.button {
-    cursor: pointer;
-    font: inherit;
-    border: none;
-    background-color: #4d1601;
-    color: white;
-    padding: 0.75rem 3rem;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    border-radius: 25px;
-    font-weight: bold;
-  }
-  
-  .button:hover,
-  .button:active {
+const Button = styled.button`
+  cursor: pointer;
+  font: inherit;
+  border: none;
+  background-color: #4d1601;
+  color: white;
+  padding: 0.75rem 3rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-radius: 25px;
+  font-weight: bold;
+
+  &:hover,
+  &:active {
     background-color: #2c0d00;
   }
-  
-  .icon {
-    width: 1.35rem;
-    height: 1.35rem;
-    margin-right: 0.5rem;
-  }
-  
-  .badge {
-    background-color: #b94517;
-    padding: 0.25rem 1rem;
-    border-radius: 25px;
-    margin-left: 1rem;
-    font-weight: bold;
-  }
-  
-  .button:hover .badge,
-  .button:active .badge {
+
+  &:hover .badge,
+  &:active .badge {
     background-color: #92320c;
   }
-  
-  .bump {
-    animation: bump 300ms ease-out;
+`;
+
+const Icon = styled.span`
+  width: 1.35rem;
+  height: 1.35rem;
+  margin-right: 0.5rem;
+`;
+
+const Badge = styled.span`
+  background-color: #b94517;
+  padding: 0.25rem 1rem;
+  border-radius: 25px;
+  margin-left: 1rem;
+  font-weight: bold;
+`;
+
+const Bump = styled.div`
+   {
+    animation: BumpAnimation 300ms ease-out;
   }
-  
-  @keyframes bump {
-    0% {
-      transform: scale(1);
-    }
-    10% {
-      transform: scale(0.9);
-    }
-    30% {
-      transform: scale(1.1);
-    }
-    50% {
-      transform: scale(1.15);
-    }
-    100% {
-      transform: scale(1);
-    }
+`;
+
+const BumpAnimation = styled.keyframes`
+  0% {
+    transform: scale(1);
   }
+  10% {
+    transform: scale(0.9);
+  }
+  30% {
+    transform: scale(1.1);
+  }
+  50% {
+    transform: scale(1.15);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
