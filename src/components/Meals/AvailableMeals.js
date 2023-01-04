ff2427f52a12b372ad9779a3d70b8484ec57e805
Scott Peat <scottpeatweb@gmail.com>
2023-01-04 09:09:05 +0000

    Finished AvailableMeals.js file

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -28,10 +30,40 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
   return (
-    <section>
+    <MealsStyle>
       <ul>{mealsList}</ul>
-    </section>
+    </MealsStyle>
   );
 };
 
 export default AvailableMeals;
+
+const MealsStyle = styled.section`
+  max-width: 60rem;
+  width: 90%;
+  margin: 2rem auto;
+  animation: meals-appear 1s ease-out forwards;
+
+  & ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+/*
+
+  @keyframes meals-appear {
+    from {
+      opacity: 0;
+      transform: translateY(3rem);
+    }
+
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+  
+`;
+*/
