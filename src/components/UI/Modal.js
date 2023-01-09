e2d87a58838d9b79f234ef09b0c1c73fe8d86ffe
Scott Peat <scottpeatweb@gmail.com>
2023-01-09 15:35:30 +0000

    Fixed Input for accessiblity and screen readers issues

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,3 +1,13 @@
-const Modal = (props) => {};
+import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const Modal = (props) => {
+    return <div className={classes.modal}>
+        <div className={}
+    </div>
+};
 
 export default Modal;
