a19cc2ea27d739e50de41c66b7b388d55a31786c
Scott Peat <scottpeatweb@gmail.com>
2023-01-25 22:39:57 +0000

    Finished the app

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -8,15 +8,29 @@ const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
   const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
-  const btnClasses = `${classes.button} ${classes.bump}`;
+  const { items } = cartCtx;
 
-  useEffect(() => {
-    setBtnIsHighlighted(true);
-  }, []);
-
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
     <button className={btnClasses} onClick={props.onClick}>
